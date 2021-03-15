415f4f3d69bc59c89990d69b1527f865d1a0b12a
eleinmaq <eleinmaq@gmail.com>
2021-03-15 15:25:10 +0200

    lab02

---
+++ js/labjava.js
@@ -0,0 +1,52 @@
+'use strict';
+let n=prompt('please Enter your name ?')
+alert('welcome '+n+' in my webpage');
+alert('please answer the following qustions with yes/no  OR  y/n');
+let qus1 = prompt('Am I elein ?').toLowerCase();
+if(qus1=='yes' || qus1=='y'){
+    console.log('the correct answer is yes ');
+    alert('the correct answer is yes');
+}
+else
+{
+    alert('your answer is incorrect')
+}
+let qus2 = prompt('Do I like Mansaf ?').toLowerCase();
+if(qus2=='yes'|| qus2=='y'){
+    console.log('the correct answer is yes');
+    alert('the correct answer is yes');
+}
+else
+{
+    alert('your answer is incorrect')
+}
+let qus3 = prompt('is my hight 160?').toLowerCase();
+if(qus3=='no'|| qus3=='n'){
+    console.log('the correct answer is no');
+    alert('the correct answer is no');
+}
+else
+{
+    alert('your answer is incorrect')
+}
+let qus4 = prompt('Do I drink coffe ?').toLowerCase();
+if(qus4=='yes'|| qus4=='y'){
+    console.log('the correct answer is yes');
+    alert('the correct answer is yes');
+}
+else
+{
+    alert('your answer is incorrect')
+}
+let qus5 = prompt('Do I like sweets ?').toLowerCase();
+if(qus5=='no'|| qus5=='n'){
+    console.log('the correct answer is no');
+    alert('the correct answer is no');
+}
+else
+{
+    alert('your answer is incorrect')
+}
+
+
+
