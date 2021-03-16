f1bf325cfdd72f013f43d0ca9e5dbefa3ff35457
eleinmaq <eleinmaq@gmail.com>
2021-03-16 16:37:06 +0200

    gi

--- js/labjava.js
+++ js/labjava.js
@@ -1,4 +1,5 @@
 'use strict';
+let sum=0;
 let n=prompt('please Enter your name ?')
 alert('welcome '+n+' in my webpage');
 alert('please answer the following qustions with yes/no  OR  y/n');
@@ -6,6 +7,7 @@ let qus1 = prompt('Am I elein ?').toLowerCase();
 if(qus1=='yes' || qus1=='y'){
     console.log('the correct answer is yes ');
     alert('the correct answer is yes');
+    sum=sum+1;
 }
 else
 {
@@ -15,6 +17,7 @@ let qus2 = prompt('Do I like Mansaf ?').toLowerCase();
 if(qus2=='yes'|| qus2=='y'){
     console.log('the correct answer is yes');
     alert('the correct answer is yes');
+    sum=sum+1;
 }
 else
 {
@@ -24,6 +27,7 @@ let qus3 = prompt('is my hight 160?').toLowerCase();
 if(qus3=='no'|| qus3=='n'){
     console.log('the correct answer is no');
     alert('the correct answer is no');
+    sum=sum+1;
 }
 else
 {
@@ -33,6 +37,7 @@ let qus4 = prompt('Do I drink coffe ?').toLowerCase();
 if(qus4=='yes'|| qus4=='y'){
     console.log('the correct answer is yes');
     alert('the correct answer is yes');
+    sum=sum+1;
 }
 else
 {
@@ -42,11 +47,52 @@ let qus5 = prompt('Do I like sweets ?').toLowerCase();
 if(qus5=='no'|| qus5=='n'){
     console.log('the correct answer is no');
     alert('the correct answer is no');
+    sum=sum+1;
 }
 else
 {
     alert('your answer is incorrect')
 }
+let qus6 = prompt('guess the day was I born in from 0-30?');
+for (let i=3 ; i>0 ; i--)
+{
+  if (qus6==27)
+  {
+    alert('Great ')
+    sum=sum+1;
+    i=0;
+  }
+  else if(qus6>27)
+  {
+    alert("You are too hight");
+    qus6 = prompt('guess the day was I born in from 0-30 ? you have just  ' +i+' time');
+  }
+  else if(qus6<27)
+  {
+    alert("You are too low");
+    qus6 = prompt('guess the day was I born in from 0-30 ? you have just' +i+' time');
+
+  }
+}
+alert('correct answer is 27 ');
+let arr=[27 , 7 , 98];
+let qus7 = prompt('guess 3 number that realted for me you will allow 6 time');
+for (let w=5 ; w>0 ; w--)
+{
+  if (qus7 == arr[0] || qus7 == arr[1] || qus7==arr[2] )
+  {
+    alert('Great')
+    sum=sum+1;
+    w=0;
+  }
+  else
+  {
+    qus7 = prompt('guess 3 number that realted for me you still have ' +w+ ' time');
+    
+  }
+}
+
+alert('You have got  '+sum+' out of 7');
 
 
 
